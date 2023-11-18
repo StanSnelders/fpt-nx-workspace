19cb575d6caba4ca7fbd2f7c9169450641884e10
StanSnelders <stansnelders@gmail.com>
2023-11-19 00:00:01 +0100

    workshop

--- apps/fpt-nx-workspace/src/app/app.component.ts
+++ apps/fpt-nx-workspace/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { FeaturesModule } from '@football-position-training/features';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, FeaturesModule],
   selector: 'fpt-nx-workspace-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
