f007921517a39f8595c742c9e8e6fb2b7be0cf8a
StanSnelders <stansnelders@gmail.com>
2023-11-22 14:31:06 +0100

    bootstrap added

--- apps/fpt-nx-workspace/src/app/app.component.ts
+++ apps/fpt-nx-workspace/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { FeaturesModule } from '@football-position-training/features';
+import { UiModule } from 'libs/football-position-training/ui/src/lib/ui.module'
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, FeaturesModule],
+  imports: [NxWelcomeComponent, RouterModule, FeaturesModule, UiModule],
   selector: 'fpt-nx-workspace-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
